e8a4ff082de337ada30dad1bb00f6c58e51e3283
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-08-12 17:36:36 -0500

    prueba

--- svelte.config.js
+++ svelte.config.js
@@ -3,33 +3,30 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
 //import adapter from '@sveltejs/adapter-auto';
-
-
-/** @type {import('@sveltejs/kit').Config} */
+ 
+ 
+ 
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),/*
-	preprocess: sveltePreprocess({
-		sass:{}
-	}),
-	base:'/test/',*/
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		prerender: {
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+    preprocess: sveltePreprocess({
+        sass:{}
+    }),
+    base:'/test/',
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        prerender: {
             entries: ['*']
-			//default: true,
-		},
-		/*paths: {
-			base: process.argv.includes('dev') ? '' : process.env.VITE_BASE_URL
-		}*/
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/<nombre-del-repositorio>' : ''
-		}
-		
-	}
+            //default: true,
+        },
+        paths: {
+            base: process.argv.includes('dev') ? '' : process.env.VITE_BASE_URL
+        }
+       
+    }
 };
-
+ 
 export default config;
