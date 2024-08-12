805794973262ee5903fe0a7d82067518e4284176
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2024-08-11 23:43:48 -0500

    pre-render

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,34 @@
-import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { sveltePreprocess } from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
+//import adapter from '@sveltejs/adapter-auto';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),/*
 	preprocess: sveltePreprocess({
 		sass:{}
 	}),
-	base:'/test/',
+	base:'/test/',*/
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
 		}),
-		paths: {
+		prerender: {
+            entries: ['*']
+			//default: true,
+		},
+		/*paths: {
 			base: process.argv.includes('dev') ? '' : process.env.VITE_BASE_URL
+		}*/
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/<nombre-del-repositorio>' : ''
 		}
+		
 	}
 };
 
